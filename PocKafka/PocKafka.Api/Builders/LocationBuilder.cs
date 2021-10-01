533c14f2680854cb794dc3ef12feb0fc97a9acab
Lucas Alcantara <lucas.alcantara@localiza.com>
2021-10-01 10:21:30 -0300

    Fix class name.

--- PocKafka/PocKafka.Api/Builders/LocationBuilder.cs
+++ PocKafka/PocKafka.Api/Builders/LocationBuilder.cs
@@ -16,7 +16,7 @@ namespace PocKafka.Api.Builders
                .RuleFor(x => x.backendArrivalDate, DateTimeOffset.Now.ToUnixTimeMilliseconds())
                .RuleFor(x => x.@event, faker => _availableEvents[faker.Random.Int(0, _availableEvents.Length - 1)])
                .RuleFor(x => x.isMoving, faker => faker.Random.Bool())
-               .RuleFor(x => x.coordinates, faker => new coordinates
+               .RuleFor(x => x.coordinates, faker => new Coordinates
                {
                    latitude = faker.Random.Double(-90.0000000d, 90.0000000d),
                    longitude = faker.Random.Double(-180.0000000, 180.0000000d),
@@ -25,19 +25,19 @@ namespace PocKafka.Api.Builders
                    heading = faker.Random.Double(),
                    altitude = faker.Random.Double(),
                })
-               .RuleFor(x => x.activity, faker => new activity
+               .RuleFor(x => x.activity, faker => new Activity
                {
                    type = _availableActivities[faker.Random.Int(0, _availableActivities.Length - 1)],
                    confidence = faker.Random.Float()
                })
-               .RuleFor(x => x.battery, faker => new battery
+               .RuleFor(x => x.battery, faker => new Battery
                {
                    isCharging = faker.Random.Bool(),
                    level = faker.Random.Float()
                })
                .RuleFor(x => x.odometer, faker => faker.Random.Double())
                .RuleFor(x => x.mock, faker => faker.Random.Bool() ? (bool?)null : faker.Random.Bool())
-               .RuleFor(x => x.extras, faker => new extras
+               .RuleFor(x => x.extras, faker => new Extras
                {
                    clientId = faker.Random.Number(min: 0, max: 99999999).ToString(),
                    licensePlate = faker.Random.String2(length: 7).ToUpper(),
